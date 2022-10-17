a5e3c62147734a8bb7c5e60ff77de2a09d67f917
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-18 01:00:36 +0530

    import @bootstrap min.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
