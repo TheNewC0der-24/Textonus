ecc6c692a5ba7292645f005fd4162d95968d1afd
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-18 01:08:28 +0530

    Create Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+    Navbar,
+    NavbarBrand
+} from 'reactstrap';
+
+import logo from '../assets/logo.svg';
+
+
+const Header = () => {
+    return (
+        <Navbar color='dark' dark={true} full={true} expand="lg" container='fluid'>
+            <NavbarBrand>
+                <img
+                    className='App-logo'
+                    alt="logo"
+                    src={logo}
+                    style={{
+                        height: 40,
+                        width: 40,
+                        marginRight: 5
+                    }}
+                />
+                Voice to Text
+            </NavbarBrand>
+        </Navbar>
+    )
+}
+
+export default Header;
